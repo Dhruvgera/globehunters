8f65c11da81a228f403b968506e1f105466a2dfb
tarun <developer@globehunters.com>
2026-06-02 15:39:17 +0100

    ALT-97 update phone number

--- src/lib/AffiliateContext.tsx
+++ src/lib/AffiliateContext.tsx
@@ -20,7 +20,7 @@ interface AffiliateContextType {
   affiliates: Affiliate[];
 }
 
-const DEFAULT_PHONE = '020 8944 4666';
+const DEFAULT_PHONE = '020 8944 4777';
 
 const AffiliateContext = createContext<AffiliateContextType>({
   affiliateCode: null,
